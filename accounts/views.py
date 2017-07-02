a549173be7d01f00412c4d838a471239ca4a6191
Kyle Chivers <chiverskyle@gmail.com>
2017-07-02 09:43:24 -0700

    completed signup functionality

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
@@ -17,6 +18,7 @@ def signup(request):
 		# success and error messages
 		error = 'Passwords didn\'t match.'
 		error_name = 'You need to provide a username.'
+		error_exists = 'That username already exists.'
 		success = 'Your account has been created!'
 		if u_name == '':
 			# throw error if no username provided
@@ -26,11 +28,16 @@ def signup(request):
 				# if passwords don't match return error
 				return render(request, 'accounts/signup.html', {'error':error})
 			else:
-				# if passwords do match create new User object
-				User.objects.create_user(username=u_name,
-										email=u_email,
-										password=u_passwrd_one)
-				return render(request, 'accounts/signup.html', {'success':success})
+				try:
+					User.objects.get(username=u_name)
+					return render(request, 'accounts/signup.html', {'error_exists':error_exists})
+				except User.DoesNotExist:
+					# if passwords do match create new User object
+					user = User.objects.create_user(username=u_name,
+											email=u_email,
+											password=u_passwrd_one)
+					login(request, user)
+					return render(request, 'accounts/signup.html', {'success':success})
 	else:
 		# if not POST method GET template
 		return render(request, 'accounts/signup.html')
