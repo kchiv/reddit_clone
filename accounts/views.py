851fe00dd6b09c22b6fbfa08a86d5d9a758b4f19
Kyle Chivers <chiverskyle@gmail.com>
2017-07-01 17:32:54 -0700

    created all logic for sign up page

--- accounts/views.py
+++ accounts/views.py
@@ -9,6 +9,28 @@ from django.contrib.auth.models import User
 
 def signup(request):
 	if request.method == 'POST':
-
+		# getting user entered info
+		u_name = request.POST['username']
+		u_email = request.POST['email']
+		u_passwrd_one = request.POST['password1']
+		u_passwrd_two = request.POST['password2']
+		# success and error messages
+		error = 'Passwords didn\'t match.'
+		error_name = 'You need to provide a username.'
+		success = 'Your account has been created!'
+		if u_name == '':
+			# throw error if no username provided
+			return render(request, 'accounts/signup.html', {'error_name':error_name})
+		else:
+			if u_passwrd_one != u_passwrd_two:
+				# if passwords don't match return error
+				return render(request, 'accounts/signup.html', {'error':error})
+			else:
+				# if passwords do match create new User object
+				User.objects.create_user(username=u_name,
+										email=u_email,
+										password=u_passwrd_one)
+				return render(request, 'accounts/signup.html', {'success':success})
 	else:
+		# if not POST method GET template
 		return render(request, 'accounts/signup.html')
