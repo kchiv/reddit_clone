377e7e0859d03195eb8b908cac8cdefe1faab1cc
Kyle Chivers <chiverskyle@gmail.com>
2017-07-03 20:10:15 -0700

    building create() method for posts app

--- posts/views.py
+++ posts/views.py
@@ -3,4 +3,12 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
+
+@login_required
+def create(request):
+	return render(request, 'posts/create.html')
+
+	
